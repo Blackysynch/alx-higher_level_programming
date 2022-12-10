392fa42eda2bb839ef4919853be1bf274381ca19
Blackysynch <ornelledavilla@yahoo.fr>
2022-12-10 07:41:24 -0800

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- lists all privileges of the MySQL users
--- on my server
+-- lists all privileges of the MySQL users in server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
